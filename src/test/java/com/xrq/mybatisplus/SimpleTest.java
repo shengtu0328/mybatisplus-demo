e1350e9e5451b9b978bd46cc7c807ce5237b4dee
rqxiao <1007689622@qq.com>
2019-05-31 17:45:12 +0800

    New changelist

--- src/test/java/com/xrq/mybatisplus/SimpleTest.java
+++ src/test/java/com/xrq/mybatisplus/SimpleTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -23,5 +24,19 @@ public class SimpleTest {
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5,userList.size());
         userList.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void insert(){
+        User user=new User();                               //id如果不设置 默认填充一个基于雪花算法的自增id
+        user.setRealName("xrq5");
+        user.setAge(23);
+        user.setManagerId(1088248166370832385l);             //默认数据库中的列名_  对应 实体类属性的驼峰
+        user.setCreateTime(LocalDateTime.now());
+
+        user.setRemark("备注111");
+        int rows= userMapper.insert(user);
+        System.out.println("影响记录数"+rows);
     }
 }
